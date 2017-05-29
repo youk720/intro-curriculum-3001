1d3aea155a980d42d32c5950fda6d43a60f58a71
youk231 <yuuki_16n1100449@nnn.ed.jp>
2017-05-29 06:31:20 +0000

    /*と*/の部分を削除しました

--- app.js
+++ app.js
@@ -7,9 +7,9 @@
  */
  function factorial(n) {
     let result = 1;
-    /* for (let i = 1; i <= n; i++) {
+    for (let i = 1; i <= n; i++) {
         result = result * i;
-    }  */
+    }  
     return result;
 }
 
