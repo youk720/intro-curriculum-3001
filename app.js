53fbcef6d7c68d3acb497954371e215b75bc90b1
youk231 <yuuki_16n1100449@nnn.ed.jp>
2017-05-29 06:30:02 +0000

    階上関数の実装

--- app.js
+++ app.js
@@ -5,9 +5,11 @@
  * @param {Number} n
  * @returns {Number}
  */
-function factorial(n) {
+ function factorial(n) {
     let result = 1;
-    // TODO このコメントを消して正しく実装してください。
+    /* for (let i = 1; i <= n; i++) {
+        result = result * i;
+    }  */
     return result;
 }
 
